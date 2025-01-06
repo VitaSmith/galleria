066f50f59fa6245464bd679c82f6d3f39ccf95f7
VitaSmith <vitasmith@rpc1.org>
2025-01-06 23:07:02 +0000

    Elementary gallery with mouse navigation and some keyboard shortcuts
    Clicking on left or right side of an image goes to previous/next.
    Supports 'F' to toggle fullscreen and 'Esc' to go back to main list.

---
+++ data.js
@@ -0,0 +1,28 @@
+const settings = {
+  fitToPage: true,
+  defaultExtension: ".svg",
+  thumbnailMaxWidth: "400px",
+  thumbnailMaxHeight: "400px",
+};
+
+const galleries = {
+  "Gallery A": {
+    numImages: 10,
+  },
+  "Gallery B": {
+    alt: "Some alternate gallery name",
+    numImages: 5,
+  },
+  "Gallery C": {
+    numImages: 10,
+  },
+  "Gallery D": {
+    numImages: 5,
+  },
+  "Gallery E": {
+    numImages: 10,
+  },
+  "Gallery F": {
+    numImages: 5,
+  },
+};
