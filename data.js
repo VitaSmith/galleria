85977ee0840872989b23e3367ae844bf4395d335
VitaSmith <vitasmith@rpc1.org>
2025-01-08 00:20:52 +0000

    Add support for custom image lists and custom extensions

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ const settings = {
   // Set to true if you want large images to be zoomed out to fit the browser window.
   // Set to false otherwise. Can also be toggled when viewing images with the <F> key.
   fitToPage: true,
-  // Image extension that Galleria assumes images to use, unless specified otherwise.
+  // Default image extension (can be overridden on a gallery by gallery basis).
   defaultExtension: ".svg",
   // Maximum default dimensions for thumbnails.
   thumbnailMaxWidth: "400px",
@@ -17,6 +17,7 @@ const settings = {
 
 const galleries = {
   "Gallery A": {
+    // Sequential gallery of 10 images starting at '1.svg' and ending at '10.svg'.
     numImages: 10,
   },
   "Gallery B": {
@@ -27,12 +28,18 @@ const galleries = {
     numImages: 10,
   },
   "Gallery D": {
-    numImages: 5,
+    // If your images have random nonsequential names, you can provide them in an imageList.
+    // Note that imageList takes precedence over all other image naming options.
+    imageList: [ "random.svg", "set.svg", "of.svg", "image.svg", "files.svg" ],
   },
   "Gallery E": {
     numImages: 10,
   },
   "Gallery F": {
+    // Default image extension for this specific gallery (overrides the one from settings).
+    defaultExtension: ".jpg",
+    // If some images don't use the default extension, list them in customExtension.
+    customExtension: [ "1.png", "4.jpeg" ],
     numImages: 5,
   },
 };
